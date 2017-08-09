e3f20f0931a420e424c841989f02675f1fc4dc12
Shubham <send2shubham@gmail.com>
2017-08-09 18:02:13 +0530

    Update App.java

--- src/main/java/t13/callableandfuture/App.java
+++ src/main/java/t13/callableandfuture/App.java
@@ -2,20 +2,21 @@ package t13.callableandfuture;
 
 /**
  * <p>
- * Use Callable and Future in Java to get results from your threads
- * and to allow your threads to throw exceptions. Plus, Future allows
- * you to control your threads, checking to see if they're running or
- * not, waiting for results and even interrupting them or descheduling them.
+ * interface Callable: A task that returns a result and may throw an exception.
+ * Just need to override call() method which Computes a result, or throws an exception if unable to do so.
+ * The Callable interface is similar to Runnable, in that both are designed for classes
+ * whose instances are potentially executed by another thread. A Runnable, however, 
+ * does not return a result and cannot throw a checked exception. 
+ *
+ * interface Future: A Future represents the result of an asynchronous computation. Methods are provided to check
+ * if the computation is complete, to wait for its completion, and to retrieve the result of the
+ * computation. The result can only be retrieved using method get when the computation has completed,
+ * blocking if necessary until it is ready. Cancellation is performed by the cancel method.
+ * Additional methods are provided to determine if the task completed normally or was cancelled.
+ * Once a computation has completed, the computation cannot be cancelled.
  * </p>
  * 
- * <p>
- * The full tutorial and the majority of the code is available at
- * https://www.udemy.com/java-multithreading/?dtcode=KmfAU1g20Sjj#/
- * </p>
- * 
- * <p>
- * @author kanastasov L1087591@live.tees.ac.uk December-2014
- * </p>
+ * <p> Udemy tutorial </p>
  */
 
 import java.io.IOException;
@@ -29,8 +30,16 @@ import java.util.concurrent.Future;
 public class App {
 
 	public static void main(String[] args) {
-		ExecutorService executor = Executors.newCachedThreadPool();
-
+		
+		/* class Executors: Factory and utility methods for Executor, ExecutorService, 
+		 ScheduledExecutorService, ThreadFactory, and Callable classes defined in this package.
+		 newCachedThreadPool: Creates a thread pool that creates new threads as needed,
+		 but will reuse previously constructed threads when they are available.
+		 interface ExecutorService: An Executor that provides methods to manage termination and
+		 methods that can produce a Future for tracking progress of one or more asynchronous tasks.
+		 */
+		ExecutorService executor = Executors.newCachedThreadPool(); 
+		
 		Future<Integer> future = executor.submit(new Callable<Integer>() {
 
 			@Override
