21e8a6187bd6851cb22df757bce77d3f29ed0cef
Shubham <send2shubham@gmail.com>
2017-10-06 00:37:13 +0530

    Update App.java

--- src/main/java/t10/reentrantLocks/App.java
+++ src/main/java/t10/reentrantLocks/App.java
@@ -12,10 +12,6 @@ package t10.reentrantLocks;
  * The full tutorial and the majority of the code is available at
  * https://www.udemy.com/java-multithreading/?dtcode=KmfAU1g20Sjj#/
  * </p>
- * 
- * <p>
- * @author kanastasov L1087591@live.tees.ac.uk December-2014
- * </p>
  */
 public class App {
 
